02ba546d3c528a9db859d98b4d85bbc4098ffd52
Oskar <o.mizgier@gmail.com>
2019-11-18 15:28:45 +0000

    Added a sample data to DB and created function for rendering each blog post from DB

---
+++ app.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var app = express();
+var bodyParser = require('body-parser');
+
+//App Config ------------------------------------------------------------------------------------------------------------
+mongoose.connect('mongodb://localhost:27017/Blog_Website', {useNewUrlParser: true, useUnifiedTopology: true});
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+//Schema Setup Mongoose Model Config ----------------------------------------------------------------------------------------------
+var blogSchema = new mongoose.Schema({
+	title: String,
+	image: String,
+	body: String,
+	created: {type: Date, default: Date.now}
+});
+
+var Blog = mongoose.model('Blog', blogSchema)
+
+	//CREATE A SAMPLE BLOG IN DATABASE
+	// Blog.create({
+	// 	title: 'Test Blog',
+	// 	image: 'https://images.unsplash.com/photo-1574012716378-0ca6f4c18c08?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1563&q=80',
+	// 	body: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?',
+	// });
+
+// RESTFUL Routes -------------------------------------------------------------------------------------------------------------------
+
+//NUMBER 1
+app.get('/', function (req, res){
+		res.redirect('/blogs')
+})
+
+// NUMBER 2
+app.get('/blogs', function (req, res){
+	Blog.find({}, function(err, blogs){
+		if(err){
+			console.log(err)
+		} else {
+			res.render('index', {blogs: blogs})
+		}
+	})
+});
+
+
+
+
+
+
+
+
+
+//Tel express to listen for requests - start server
+app.listen(3000, function() { 
+  console.log('Server listening on port 3000'); 
+});
