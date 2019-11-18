58c16b8c8e7660f0e3f8e1a33745af5fd1a94b9f
Oskar <o.mizgier@gmail.com>
2019-11-18 16:12:56 +0000

    Added the post request and possibility to add a new blog that is being saved to the DB and displayed

--- app.js
+++ app.js
@@ -44,6 +44,24 @@ app.get('/blogs', function (req, res){
 	})
 });
 
+// NUMBER 3 ADD A NEW BLOG
+app.get('/blogs/new', function (req, res){
+	res.render('new')
+});
+
+// NUMBER 4 POST A NEW BLOG
+app.post('/blogs', function (req, res){
+	
+	//create new blog
+	Blog.create(req.body.blog, function(err, newBlog){
+		if(err){
+			console.log(err)
+		} else {
+			res.redirect('/blogs')
+		}
+	})
+})
+
 
 
 
